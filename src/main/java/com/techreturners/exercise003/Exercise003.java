05442b6084e66017fed3f351eba2fcb74d056f36
mel4782gh <34482153+mel4782gh@users.noreply.github.com>
2021-09-02 12:40:58 +0100

    implement exercise003 - 4 tests

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,28 @@
 package com.techreturners.exercise003;
 
+import java.util.ArrayList;
+
 public class Exercise003 {
+    //declare string array to store all ice cream flavours
+    private String[] iceCreamFlavours = {"Pistachio","Raspberry Ripple","Vanilla","Mint Chocolate Chip","Chocolate","Mango Sorbet"};
 
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        int code=0;
+        //iterate iceCreamFlavours to see if match to iceCreamFlavour
+        for(int i=0;i<iceCreamFlavours.length;i++){
+            if (iceCreamFlavour.equals(iceCreamFlavours[i])){
+                //store the code for the flavour
+                code = i;
+            }
+            // should I throw an exception here if there is no matching flavour?
+        }
+        return code;
+
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return iceCreamFlavours;
+
     }
 
 }
